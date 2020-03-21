ab997c16e6c0ccd1e9727f9d94e5ea5472befb26
Bill-M123 <bill_hk2002@yahoo.com.hk>
2020-03-21 13:34:23 -0400

    Small class change

--- poker_classes/dealer.py
+++ poker_classes/dealer.py
@@ -4,6 +4,7 @@ class Dealer():
 
     def __init__(self):
         self.new_deck=[(rank, suit) for rank in range(1,14) for suit in ['S','H','C','D']]
+        self.dealer_position=0
 
     def shuffle_deck(self,deck):
         random.shuffle(deck)
