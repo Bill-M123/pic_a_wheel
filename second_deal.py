94c39ee22896d7e15839051232ce72cc31addb44
Bill-M123 <bill_hk2002@yahoo.com.hk>
2020-04-08 00:14:49 -0400

    Ranker fixed

--- second_deal.py
+++ second_deal.py
@@ -68,6 +68,17 @@ def new_deal():
             combos=dealer.get_possible_hands(tmp_hand,flat_list)
             for c in combos:
                 tmp_high,tmp_low=dealer.rank_hands((c))
+
+                sorting_dict={1:'01',2:'02',3:'03',4:'04',5:'05',
+                        6:'06',7:'07',8:'08',9:'09',10:'10',
+                        11:'11',12:'12',13:'13',14:'14',15:'15'}
+
+                
+                tmp_high_c=[sorting_dict[x] for x in tmp_high[2]]
+                tmp_low_c=[sorting_dict[x] for x in tmp_low[2]]
+                tmp_high=(tmp_high[0],tmp_high[1],tmp_high_c,tmp_high[3])
+                tmp_low=(tmp_low[0],tmp_low[1],tmp_low_c,tmp_low[3])
+
                 high_hand_ranks.append(tmp_high)
                 low_hand_ranks.append(tmp_low)
             #print('High/Low',high_hand_ranks[0],low_hand_ranks[0])
@@ -84,7 +95,7 @@ def new_deal():
             #print('Low_hands','\n',low_hand_ranks[:3],'\n',low_hand_ranks[-3:],'\n')
 
 
-        
+
         display_dict=dealer.add_to_display_dict(display_dict,i,p,cards)
     #print(dealer.common_cards)
     common_dict=dealer.make_common_display_dict(dealer.common_cards,cards)
