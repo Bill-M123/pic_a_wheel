d053d087557fcf4af2af7f4774ebfeda401d82a9
Bill-M123 <bill_hk2002@yahoo.com.hk>
2020-05-28 00:17:18 -0400

    Twelveth_deal working

--- twelveth_deal.py
+++ twelveth_deal.py
@@ -858,5 +858,5 @@ def master_control():
         return f"You are not Bornstein or Clyde, Fuck Off!"
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', debug=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+    #app.run(debug=True)
